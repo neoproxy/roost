7a6f9543a5a5943fdd8715a72c7a41c437666f8b
root <root@zu.8bitpipe.com>
2018-08-16 16:22:10 +0000

    add main program, app.py and article data, data.py

---
+++ app.py
@@ -0,0 +1,38 @@
+#!flask/bin/python
+from flask import Flask, render_template
+from data import Articles
+
+app = Flask(__name__)
+
+Articles = Articles()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/npc')
+def npc():
+    return render_template('npc.html')
+
+@app.route('/traps')
+def traps():
+    return render_template('traps.html')
+
+@app.route('/tavern')
+def tavern():
+    return render_template('tavern.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/articles/<string:id>/')
+def article(id):
+    return render_template('article.html', id = id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
